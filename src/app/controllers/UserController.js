01553fbe0b37dafcbf7041951ba228db8d73d800
llucasmota <lucas.o.mota@gmail.com>
2019-09-25 18:56:44 -0300

    Primeir commit do sistema schedule-vacation, que servirá para realizar marcação de férias de empregados

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  cpf: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import routes from "./routes";
+import databaseConfig from "./app/database/index";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+mongoose.createConnection(databaseConfig.url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+// subindo o server
+app.listen(3333);
