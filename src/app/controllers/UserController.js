b5f32c155c425467787649fa9cc5652926e46a82
llucasmota <lucas.o.mota@gmail.com>
2019-09-25 19:01:26 -0300

    Corrigindo estrutura das pastas

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,27 +1,3 @@
-import mongoose from "mongoose";
+import User from "../models/Users";
 
-const User = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  cpf: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+class UserController {}
