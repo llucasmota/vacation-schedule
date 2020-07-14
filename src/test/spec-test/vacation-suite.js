2dc08c9d6306b92f692650c704415fc93f0e4790
Lucas Mota <lucas.o.mota@gmail.com>
2020-07-14 09:34:49 -0300

    Iniciando manutenção/criação dos scripts de teste

--- src/test/spec-test/vacation-suite.js
+++ src/test/spec-test/vacation-suite.js
@@ -1,20 +1,24 @@
-const { describe, it } = require('mocha');
+const { describe, it, beforeEach, before } = require('mocha');
 const express = require('express');
 
 const request = require('supertest');
 
-const app = express();
+const app = 'http://localhost:3333';
 
 describe('Deve ser possível criar um usuário', () => {
-  it('POST /users', () => {
+  it('POST /users', done => {
     request(app)
       .post('/users')
       .send({
         name: 'Guts',
-        cpf: '02912345678',
+        cpf: '02912345618',
         email: 'guts.berserk@gmail.com',
         password: '123456',
       })
-      .expect(200);
+      .expect(200)
+      .end(err => {
+        if (err) return done(err);
+        return done();
+      });
   });
 });
